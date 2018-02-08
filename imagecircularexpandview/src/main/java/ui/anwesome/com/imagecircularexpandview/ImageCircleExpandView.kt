b37efec37556310736420ec5db201050c01637c8
Anwesh43 <anweshthecool0@gmail.com>
2018-02-08 07:40:56 +0530

    adding ImageCircleExpandView

--- imagecircularexpandview/src/main/java/ui/anwesome/com/imagecircularexpandview/ImageCircleExpandView.kt
+++ imagecircularexpandview/src/main/java/ui/anwesome/com/imagecircularexpandview/ImageCircleExpandView.kt
@@ -3,3 +3,15 @@ package ui.anwesome.com.imagecircularexpandview
 /**
  * Created by anweshmishra on 08/02/18.
  */
+import android.graphics.*
+import android.content.*
+import android.view.*
+class ImageCircleExpandView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        return true
+    }
+}
