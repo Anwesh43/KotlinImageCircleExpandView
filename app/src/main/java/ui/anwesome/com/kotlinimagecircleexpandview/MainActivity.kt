dc2a20d1a8c798f7c95de9368a4f3d001f8c01ea
Anwesh43 <anweshthecool0@gmail.com>
2018-02-08 08:36:25 +0530

    using ImageCircularExpandView in app

--- app/src/main/java/ui/anwesome/com/kotlinimagecircleexpandview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinimagecircleexpandview/MainActivity.kt
@@ -1,12 +1,14 @@
 package ui.anwesome.com.kotlinimagecircleexpandview
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.imagecircularexpandview.ImageCircleExpandView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ImageCircleExpandView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
     }
 }
